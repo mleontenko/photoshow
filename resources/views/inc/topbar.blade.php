94d59c8f092c58360c92ad06e9370a4dd45d4c40
Marin Leontenko <marin.leontenko@gmail.com>
2018-01-26 15:36:13 +0100

    append topbar

--- resources/views/inc/topbar.blade.php
+++ resources/views/inc/topbar.blade.php
@@ -2,7 +2,8 @@
     <div class="row">
         <div class="top-bar-left">
             <ul class="menu">
-                <li class="menu-text">Home</li>
+                <li class="menu-text">PhotoShow</li>
+                <li><a href="/">Home</a></li>
                 <li><a href="/albums/create">Create Album</a></li>
             </ul>
         </div>
